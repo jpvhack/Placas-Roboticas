0f5c03fa4dc6f0ac84568867b6adf92b920e6949
DESKTOP-15JV8VG\Alumne <pmshot@hotmail.com>
2020-11-06 12:47:26 +0100

    no message

--- 8_LED_con_74HC595_ASCII_a_binario/8_LED_con_74HC595_ASCII_a_Binario.ino
+++ 8_LED_con_74HC595_ASCII_a_binario/8_LED_con_74HC595_ASCII_a_Binario.ino
@@ -2,7 +2,7 @@ int latchPin = 11;      // (11) ST_CP [RCK] on 74HC595
 int clockPin = 9;      // (9) SH_CP [SCK] on 74HC595
 int dataPin = 12;     // (12) DS [S1] on 74HC595
 
-byte leds = 196; // numero entre 0 i 255 que el programa mostra el seu binari en forma de leds encesos o apagats
+byte leds = 196; // numero entre 0 i 255 que el programa mostra el seu binari en forma de leds encesos o apagats. 196 representa la letra A
 
 void updateShiftRegister()
 {
